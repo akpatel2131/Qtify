9efc59e26116985c8e0a63c5633d62465760a62d
akpatel2131 <akpatel2131@gmail.com>
2023-09-10 10:27:51 +0530

    C5: Hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Navbar from "./Pages/Navbar/Navbar";
-// import HeroSection from "./Pages/HeroSection/HeroSection";
+import HeroSection from "./Pages/HeroSection/HeroSection";/
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <HeroSection /> */}
+      <HeroSection />
     </div>
   );
 }
